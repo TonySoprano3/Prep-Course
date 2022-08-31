c3ee6f4fd753592eebc4632ecb685b3f2d440ce4
TonySoprano3 <lautarovirtual@hotmail.com>
2022-08-31 00:09:16 -0300

    Día 1 final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -135,13 +135,13 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return Math.pow(num, 2);
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return Math.pow(num, 3);
 }
 
 function elevar(num, exponent) {
